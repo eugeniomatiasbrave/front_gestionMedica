b2c8df1835e0056833a64d852af93a0b8e3378da
Eugenio Matias Brave <eugenio_m_brave@hotmail.com>
2025-10-05 16:42:51 -0300

    adapter vercel runtime nodejs 20

--- svelte.config.js
+++ svelte.config.js
@@ -4,17 +4,16 @@ import adapterNode from '@sveltejs/adapter-node';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapterVercel({
-			runtime: 'nodejs18.x',
-			regions: ['iad1'], 
-			split: false // mantiene todas las rutas en una sola función por defecto
-		}) || adapterNode(),
+		adapter:
+			adapterVercel({
+				runtime: 'nodejs20.x',
+				regions: ['iad1'],
+				split: false
+			}) || adapterNode(),
 		files: {
-            assets: 'static'
-        }
+			assets: 'static'
+		}
 	}
 };
 
 export default config;
-
-// ok
